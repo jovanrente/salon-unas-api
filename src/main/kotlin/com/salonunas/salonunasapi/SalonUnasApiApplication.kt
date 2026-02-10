f5a29d62a5df885ddb09fcf4fe25505305db6397
jovan renteria <jovan.renteria@itxpertix.com>
2026-02-09 19:23:23 -0500

    Initial commit - Proyecto Spring Boot Kotlin

---
+++ src/main/kotlin/com/salonunas/salonunasapi/SalonUnasApiApplication.kt
@@ -0,0 +1,11 @@
+package com.salonunas.salonunasapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalonUnasApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SalonUnasApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/salonunas/salonunasapi/SalonUnasApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.salonunas.salonunasapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalonUnasApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
